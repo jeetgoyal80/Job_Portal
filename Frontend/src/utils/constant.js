655bf488af07128491f4706588aae83ea6c1b0a3
jeetgoyal80 <jeetgoyal178@gmail.com>
2025-06-23 19:44:26 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const EndPointUserURL = `${import.meta.env.Vite_backent_url}/api/user`
-export const EndPointUserURLJOB = `${import.meta.env.Vite_backent_url}/api/user/job`
-export const EndPointUserURLAPPLICATION = `${import.meta.env.Vite_backent_url}/api/applications`
-export const EndPointUserURLCOMPANY = `${import.meta.env.Vite_backent_url}/api/user/company`
+export const EndPointUserURL = `${import.meta.env.VITE_BACKEND_URL}/api/user`
+export const EndPointUserURLJOB = `${import.meta.env.VITE_BACKEND_URL}/api/user/job`
+export const EndPointUserURLAPPLICATION = `${import.meta.env.VITE_BACKEND_URL}/api/applications`
+export const EndPointUserURLCOMPANY = `${import.meta.env.VITE_BACKEND_URL}/api/user/company`
